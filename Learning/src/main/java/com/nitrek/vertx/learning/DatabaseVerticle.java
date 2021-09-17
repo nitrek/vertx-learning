8f23606ca7d3276c3d53655c68838849b3b1e49b
nitrek <koolestnitesh@gmail.com>
2021-09-18 00:20:04 +0530

    updates for added db:

--- Learning/src/main/java/com/nitrek/vertx/learning/DatabaseVerticle.java
+++ Learning/src/main/java/com/nitrek/vertx/learning/DatabaseVerticle.java
@@ -1,38 +1,83 @@
 package com.nitrek.vertx.learning;
 
+import java.util.function.Function;
+
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.FlywayException;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.sql.SQLClient;
+import io.vertx.ext.sql.SQLConnection;
 
 public class DatabaseVerticle extends AbstractVerticle {
 
+  public static final String LIST_ALL_TODOS_ADDR = "com.nitrek.demo.vertx.list_all_todos";
+  public static final String ADD_TODO_ADD = "com.nitrek.demo.vertx.add_todo";
+  public static final String GET_TODO_BY_ID_ADDR = "com.nitrek.demo.vertx.get_todo_by_id";
+  public static final String UPDATE_TODO_ADDR = "com.nitrek.demo.vertx.update_todo";
+
+  private static final String LIST_ALL_TODOS = "SELECT * FROM todos ORDER BY created ASC";
+  private static final String GET_TODO_BY_ID = "SELECT * FROM todos WHERE id = ?";
+  private static final String UPDATE_TODO = "UPDATE todos SET title = ?, description = ?, due_date = ?, complete = ? WHERE id = ? RETURNING *";
+  private static final String ADD_TODO = "INSERT INTO todos (title, description, due_date, complete) VALUES (?, ?, ?, ?) RETURNING *";
+  SQLClient client;
+
   @Override
   public void start(Promise<Void> start) throws Exception {
-    doDatabaseMigrations()
-      .setHandler(start::handle);
+    doDatabaseMigrations().compose(this::configureSqlClient).setHandler(start::handle);
+  }
+
+  Future<Void> configureSqlClient(Void unused) {
+    client = JDBCClient.createShared(vertx, config().getJsonObject("db"));
+    return Promise.<Void>succeededPromise().future();
+  }
+
+  Future<ResultSet> queryWithParams(SQLConnection conn, String query, JsonArray params) {
+
+    return Future.<ResultSet>future(promise -> {
+      conn.queryWithParams(query, params, promise);
+    });
+  }
+
+  Future<JsonObject> mapToFirstResult(ResultSet rs) {
+
+    if (rs.getNumRows() >= 0) {
+      return Future.<JsonObject>succeededFuture(rs.getRows().get(0));
+    } else {
+      return Future.<JsonObject>failedFuture("No rows found");
+    }
+  }
+
+  Future<JsonArray> mapToJsonArray(ResultSet rs) {
+    return Promise.succeededPromise(new JsonArray(rs.getRows())).future();
   }
 
   /**
-   * Uses the {@code loadedConfig} to try to perform the required database schema migrations
+   * Uses the {@code loadedConfig} to try to perform the required database schema
+   * migrations
+   * 
    * @param unused A {@link Void} object which is ignored
-   * @return A {@link Future} which indicates the success/failure of this operation
+   * @return A {@link Future} which indicates the success/failure of this
+   *         operation
    */
   Future<Void> doDatabaseMigrations() {
-      JsonObject dbConfig = config().getJsonObject("db", new JsonObject());
-      String url = dbConfig.getString("url", "jdbc:postgresql://127.0.0.1:5432/introduction");
-      String adminUser = dbConfig.getString("admin_user", "introduction");
-      String adminPass = dbConfig.getString("admin_pass", "introduction");
-      Flyway flyway = Flyway.configure().dataSource(url, adminUser, adminPass).load();
+    JsonObject dbConfig = config().getJsonObject("db", new JsonObject());
+    String url = dbConfig.getString("url", "jdbc:postgresql://127.0.0.1:5432/introduction");
+    String adminUser = dbConfig.getString("admin_user", "introduction");
+    String adminPass = dbConfig.getString("admin_pass", "introduction");
+    Flyway flyway = Flyway.configure().dataSource(url, adminUser, adminPass).load();
 
-      try {
-          flyway.migrate();
-          return Promise.<Void>succeededPromise().future();
-      } catch (FlywayException fe) {
-          return Promise.<Void>failedPromise(fe).future();
-      }
+    try {
+      flyway.migrate();
+      return Promise.<Void>succeededPromise().future();
+    } catch (FlywayException fe) {
+      return Promise.<Void>failedPromise(fe).future();
+    }
   }
 }
--- Learning/src/main/java/com/nitrek/vertx/learning/MainVerticle.java
+++ Learning/src/main/java/com/nitrek/vertx/learning/MainVerticle.java
@@ -19,29 +19,23 @@ public class MainVerticle extends AbstractVerticle {
     public void start(Promise<Void> start) {
         // Sequential Composition - Do A, Then B, Then C . . . . Handle errors
         // https://vertx.io/docs/vertx-core/java/#_sequential_composition
-        doConfig()
-            .compose(this::storeConfig)
-            .compose(this::deployOtherVerticles)
-            .setHandler(start::handle);
+        doConfig().compose(this::storeConfig).compose(this::deployOtherVerticles).setHandler(start::handle);
     }
 
     /**
-     * Set up and execute the {@link ConfigRetriever} to load the configuration for the application
-     * @param start The {@link Promise} which is to be resolved as this Verticle loads
+     * Set up and execute the {@link ConfigRetriever} to load the configuration for
+     * the application
+     * 
+     * @param start  The {@link Promise} which is to be resolved as this Verticle
+     *               loads
      * @param router The {@link Router} for the REST API paths
      */
     Future<JsonObject> doConfig() {
-        ConfigStoreOptions defaultConfig = new ConfigStoreOptions()
-                .setType("file")
-                .setFormat("json")
+        ConfigStoreOptions defaultConfig = new ConfigStoreOptions().setType("file").setFormat("json")
                 .setConfig(new JsonObject().put("path", "config.json"));
-        ConfigStoreOptions cliConfig = new ConfigStoreOptions()
-                .setType("json")
-                .setConfig(config());
+        ConfigStoreOptions cliConfig = new ConfigStoreOptions().setType("json").setConfig(config());
 
-        ConfigRetrieverOptions opts = new ConfigRetrieverOptions()
-                .addStore(defaultConfig)
-                .addStore(cliConfig);
+        ConfigRetrieverOptions opts = new ConfigRetrieverOptions().addStore(defaultConfig).addStore(cliConfig);
 
         ConfigRetriever cfgRetriever = ConfigRetriever.create(vertx, opts);
 
@@ -50,8 +44,10 @@ public class MainVerticle extends AbstractVerticle {
 
     /**
      * Store loaded configuration for use in subsequent operations
+     * 
      * @param config The configuration loaded via Vert.x Config
-     * @return A {@link Future} of type {@link Void} indication the success or failure of this operation
+     * @return A {@link Future} of type {@link Void} indication the success or
+     *         failure of this operation
      */
     Future<Void> storeConfig(JsonObject config) {
         loadedConfig.mergeIn(config);
@@ -61,13 +57,16 @@ public class MainVerticle extends AbstractVerticle {
     /**
      * Deploy our other {@link io.vertx.core.Verticle}s in concurrently
      * https://vertx.io/docs/vertx-core/java/#_concurrent_composition
+     * 
      * @param unused A {@link Void} instance (Not used in this method)
-     * @return A {@link Future} which is resolved once both of the Verticles are deployed
+     * @return A {@link Future} which is resolved once both of the Verticles are
+     *         deployed
      */
     Future<Void> deployOtherVerticles(Void unused) {
         DeploymentOptions opts = new DeploymentOptions().setConfig(loadedConfig);
 
-        Future<String> dbVerticle = Future.future(promise -> vertx.deployVerticle(new DatabaseVerticle(), opts, promise));
+        Future<String> dbVerticle = Future
+                .future(promise -> vertx.deployVerticle(new DatabaseVerticle(), opts, promise));
         Future<String> webVerticle = Future.future(promise -> vertx.deployVerticle(new WebVerticle(), opts, promise));
         Future<String> helloGroovy = Future.future(promise -> vertx.deployVerticle("Hello.groovy", opts, promise));
         Future<String> helloJs = Future.future(promise -> vertx.deployVerticle("Hello.js", opts, promise));
